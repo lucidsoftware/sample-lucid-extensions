722126dca80a484953056c964f46acdb53becf00
Sheyne Anderson <sheyne@sheyne.com>
2023-05-22 11:51:44 -0600

    Migrate to latest extension-sdk

--- asasa/data-connector/debug-server.ts
+++ asasa/data-connector/debug-server.ts
@@ -1,24 +1,7 @@
-import {DataConnectorClient} from "lucid-extension-sdk";
-import {signatureValidatorImportedNode} from "lucid-extension-sdk/dataconnector/signaturevalidator";
+import {DataConnectorClient} from "lucid-extension-sdk/dataconnector/dataconnectorclient";
 import * as crypto from "crypto";
-import {makeDataConnector} from "./index"
 import * as express from "express";
+import {makeDataConnector} from "./index"
 
-// Temporary workaround for crypto import issue
-signatureValidatorImportedNode.crypto = crypto;
-
-const app = express();
-
-// Set CORS to allow requests from lucid.app
-app.use((req: any, res: any, next: () => void) => {
-    if (req.headers?.origin?.match(/.lucid.app$/)) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        res.header('Vary', 'Origin');
-        res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-        res.header('Access-Control-Allow-Headers', '*');
-    }
-    next();
-});
-
-const dataConnector = makeDataConnector(new DataConnectorClient());
-dataConnector.runDebugServer({express, app, port: 3001});
+const dataConnector = makeDataConnector(new DataConnectorClient({crypto, Buffer}));
+dataConnector.runDebugServer({express, port: 3001});
