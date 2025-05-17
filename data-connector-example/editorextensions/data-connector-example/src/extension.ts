8fe88d4d9292a098bcfd376927a9443a25501e22
Egor Kraev <egor.kraev@gmail.com>
2025-05-17 16:58:51 +0200

    Edits in shapes correctly feed back to edits in data source

--- data-connector-example/editorextensions/data-connector-example/src/extension.ts
+++ data-connector-example/editorextensions/data-connector-example/src/extension.ts
@@ -1,7 +1,9 @@
 // Import from the refactored modules
 import { setupTextEditHook } from "./textEditHandler";
 import { setupMenus } from "./menuSetup";
+import { setupLineDragHandler } from "./lineDragHandler";
 
 // Initialize the extension
 setupTextEditHook();
 setupMenus();
+setupLineDragHandler();
